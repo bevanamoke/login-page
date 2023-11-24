6e8cf82665f16b9ebed41919fca78200c2fba4e4
bevan <bevanamoke@gmail.com>
2023-11-24 20:36:58 +0300

    added javascript functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inputs = document.querySelectorAll(".input-field");
 const toggle_btn = document.querySelectorAll(".toggle");
 const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image")
 
 inputs.forEach((inp) => {
     inp.addEventListener("focus", () => {
@@ -17,3 +19,24 @@ toggle_btn.forEach((btn) => {
         main.classList.toggle("sign-up-mode");
     });
 });
+
+function moveSlider() {
+    let index = this.dataset.value;
+
+    let currentImage = document.querySelector(`.img-${index}`);
+    images.forEach((img) => img.classList.remove("show"));
+    currentImage.classList.add("show");
+
+    const textSlider = document.querySelector(".text-group");
+    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+
+
+    bullets.forEach(bull => bull.classList.remove("active"));
+    this.classList.add("active");
+};
+
+bullets.forEach((bullet) => {
+    bullet.addEventListener("click", moveSlider)
+}
+);
